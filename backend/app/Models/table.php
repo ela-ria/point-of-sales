d09e3c2bc52a6738a47def112dfd89fe76e4fa5b
JaymartImp <jaymartimpas0@gmail.com>
2026-04-28 16:04:42 +0000

    feat: auth & user management module

--- backend/app/Models/table.php
+++ backend/app/Models/table.php
@@ -1,10 +1,8 @@
 <?php
 
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
 
 return new class extends Migration
 {
@@ -14,7 +12,6 @@ return new class extends Migration
             $table->enum('role', ['cashier', 'supervisor', 'admin'])->default('cashier')->after('email');
         });
     }
-
 
     public function down(): void
     {
